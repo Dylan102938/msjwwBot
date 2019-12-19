da979e36332d3a28ff0c267e6d4e4253e6d1a0af
sharora <sharora@gmail.com>
2019-12-19 14:15:37 -0800

    idk what changed but local shit workin

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ def main():
 	pauseseconds = random.randrange(60, 90, 1)
 
 	while(True):
-		bot.upload_photo("./pics/msjww2.jpg", caption="#msjww19")
+		bot.upload_photo("pics/msjww2.jpg", caption="#msjww19 v2 shit oh yea yea")
 		time.sleep(pauseseconds)
 		pauseseconds = random.randrange(60, 90, 1)
 		print("Waiting: ", pauseseconds)
 
-		os.system("rm ./pics/msjww2.jpg.REMOVE_ME")
-		os.system("cp ./pics/msjww1.jpg ./pics/msjww2.jpg")
+		os.system("rm pics/msjww2.jpg.REMOVE_ME")
+		os.system("cp pics/msjww1.jpg pics/msjww2.jpg")
 
 if __name__ == "__main__":
 	main()
