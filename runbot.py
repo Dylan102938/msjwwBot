e474b806a9e4d906e1bf9a288d773ac672cd355e
Dylan102938 <dfeng102938@gmail.com>
2019-12-19 15:56:44 -0800

    Fixed Windows + no obvious spam line

--- runbot.py
+++ runbot.py
@@ -3,20 +3,25 @@ import time
 import random
 import os
 
+
 def main():
 	bot = instabot.Bot()
 	bot.login()
 
 	pauseseconds = random.randrange(60, 90, 1)
+	os.system("cp msjww.jpg msjwwcopy.jpg")
 
 	while(True):
-		bot.upload_photo("pics/msjww2.jpg", caption="#msjww19 v2 shit oh yea yea")
-		time.sleep(pauseseconds)
-		pauseseconds = random.randrange(60, 90, 1)
+		bot.upload_photo("msjwwcopy.jpg", caption="#msjww19")
 		print("Waiting: ", pauseseconds)
 
-		os.system("rm pics/msjww2.jpg.REMOVE_ME")
-		os.system("cp pics/msjww1.jpg pics/msjww2.jpg")
+		time.sleep(pauseseconds)
+		pauseseconds = random.randrange(60, 90, 1)
+
+		os.system("rm msjwwcopy.jpg.REMOVE_ME")
+		os.system("cp msjww.jpg msjwwcopy.jpg")
+
+
 
 if __name__ == "__main__":
 	main()
